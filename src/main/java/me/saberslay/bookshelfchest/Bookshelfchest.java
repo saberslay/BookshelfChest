cf4051377e25c95e09aedf767d63c521fef611b8
Saberslay <dansteamshell@gmail.com>
2024-07-24 13:27:49 +0100

    Project setup

---
+++ src/main/java/me/saberslay/bookshelfchest/Bookshelfchest.java
@@ -0,0 +1,17 @@
+package me.saberslay.bookshelfchest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Bookshelfchest extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
