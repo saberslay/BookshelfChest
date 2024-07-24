527640c3dcb630de6c14016926581483447e9abb
Saberslay <dansteamshell@gmail.com>
2024-07-24 15:24:06 +0100

    There is an Error when loading the saved data

---
+++ src/main/java/me/saberslay/bookshelfchest/Manegers/EventManager.java
@@ -0,0 +1,14 @@
+import me.saberslay.bookshelfchest.Bookshelfchest;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class EventManager implements Listener {
+    public EventManager(Bookshelfchest Bookshelfchest) {
+    }
+    @EventHandler
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
+
+    }
+
+}
